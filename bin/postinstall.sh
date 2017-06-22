46755fff341183f807202947b73e97c4672ac375
Trilleplay <trille@trilleplay.net>
2017-06-22 15:15:23 +0200

    Update postinstall.sh

--- bin/postinstall.sh
+++ bin/postinstall.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Set Minecraft version
-MC_VERSION="${MC_VERSION:-1.9}"
+MC_VERSION="${MC_VERSION:-1.12}"
 
 
 # Download Minecraft
-curl -o vendor/minecraft_server.jar "https://s3.amazonaws.com/Minecraft.Download/versions/1.9.4/minecraft_server.1.9.4.jar"
+curl -o vendor/minecraft_server.jar "https://s3.amazonaws.com/Minecraft.Download/versions/1.12/minecraft_server.1.12.jar"
